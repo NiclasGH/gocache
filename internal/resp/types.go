abdc53e9e6ba2004d6a39d685855d8418bd2a1ee
Niclas Kürschner <niclas.kuerschner@outlook.com>
2025-10-12 14:21:49 +0200

    feat: add writer

--- internal/resp/types.go
+++ internal/resp/types.go
@@ -12,13 +12,3 @@ var (
 	BULK    = Typ { RespCode: '$', Name: "Error" }
 	ARRAY   = Typ { RespCode: '*', Name: "Error" }
 )
-
-// This can be improved using union types, which go currently do not support
-type Value struct {
-	Typ   string
-	Str   string
-	Num   int
-	Bulk  string
-	Array []Value
-}
-
