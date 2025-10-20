7b9d8084b6444a437f502d174bfeebfc7a7ab95d
Niclas Kürschner <niclas.kuerschner@outlook.com>
2025-10-20 11:03:48 +0200

    feat: enable main integration tests

--- cmd/gocache/main_test.go
+++ cmd/gocache/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gocache/internal/core/resp"
 	"net"
 	"os"
 	"testing"
@@ -124,70 +125,69 @@ func Test_setAndGet(t *testing.T) {
 	}
 }
 
-// TODO enable test when startup use case exists
-// func Test_getWithInitialization(t *testing.T) {
-// 	// PREPARATIONS
-// 	port := "8103"
-// 	file, err := prepare(port)
-// 	if err != nil {
-// 		t.Error(err)
-// 		return
-// 	}
-// 	defer os.Remove(file.Name())
-//
-// 	request := resp.Value{
-// 		Typ: resp.ARRAY.Typ,
-// 		Array: []resp.Value{
-// 			{
-// 				Typ:  resp.BULK.Typ,
-// 				Bulk: "SET",
-// 			},
-// 			{
-// 				Typ:  resp.BULK.Typ,
-// 				Bulk: "Tira",
-// 			},
-// 			{
-// 				Typ:  resp.BULK.Typ,
-// 				Bulk: "Misu",
-// 			},
-// 		},
-// 	}
-// 	file.Write(request.Marshal())
-//
-// 	// STEP 1: Starting the server
-// 	client, err := setupServer(port)
-// 	if err != nil {
-// 		t.Error(err)
-// 		return
-// 	}
-// 	defer client.Close()
-//
-// 	// STEP 2: Call Get
-// 	err = func() error {
-// 		// given
-// 		request := "*2\r\n$3\r\nGET\r\n$4\r\nTira\r\n"
-// 		expected := "$4\r\nMisu\r\n"
-//
-// 		// when
-// 		client.Write([]byte(request))
-//
-// 		// then
-// 		buf := make([]byte, 1024)
-// 		length, err := client.Read(buf)
-// 		if err != nil {
-// 			t.Error("The client was not able to read the handled connection response")
-// 		}
-// 		res := string(buf[:length])
-//
-// 		assert.Equal(t, expected, res)
-//
-// 		return nil
-// 	}()
-// 	if err != nil {
-// 		t.Error(err)
-// 		return
-// 	}
-// }
+func Test_getWithInitialization(t *testing.T) {
+	// PREPARATIONS
+	port := "8103"
+	file, err := prepare(port)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer os.Remove(file.Name())
+
+	request := resp.Value{
+		Typ: resp.ARRAY.Typ,
+		Array: []resp.Value{
+			{
+				Typ:  resp.BULK.Typ,
+				Bulk: "SET",
+			},
+			{
+				Typ:  resp.BULK.Typ,
+				Bulk: "Tira",
+			},
+			{
+				Typ:  resp.BULK.Typ,
+				Bulk: "Misu",
+			},
+		},
+	}
+	file.Write(request.Marshal())
+
+	// STEP 1: Starting the server
+	client, err := setupServer(port)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer client.Close()
+
+	// STEP 2: Call Get
+	err = func() error {
+		// given
+		request := "*2\r\n$3\r\nGET\r\n$4\r\nTira\r\n"
+		expected := "$4\r\nMisu\r\n"
+
+		// when
+		client.Write([]byte(request))
+
+		// then
+		buf := make([]byte, 1024)
+		length, err := client.Read(buf)
+		if err != nil {
+			t.Error("The client was not able to read the handled connection response")
+		}
+		res := string(buf[:length])
+
+		assert.Equal(t, expected, res)
+
+		return nil
+	}()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
 
 func prepare(port string) (*os.File, error) {
 	f, err := os.CreateTemp("", "database.test.aof")
